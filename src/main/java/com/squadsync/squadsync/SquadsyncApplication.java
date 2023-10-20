166ee1561d3ce6cc1c24e37dddcacb7b48bd7bd9
bbubb <brandoncbunch@gmail.com>
2023-10-20 11:20:21 -0500

    initial project setups using Spring Initializr and with conventional project structure directories added

---
+++ src/main/java/com/squadsync/squadsync/SquadsyncApplication.java
@@ -0,0 +1,13 @@
+package com.squadsync.squadsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquadsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquadsyncApplication.class, args);
+	}
+
+}
